b6d4fdd45f4abdadf601ff85f5ee423bf97e5322
Luis Garcia <luisfernando.garcia@inswitch.com>
2024-10-31 01:30:00 -0600

    Refactor layout component to use Gluestack UI provider and add wallet routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,8 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { GluestackUIProvider} from "@gluestack-ui/themed"
+import { config } from "@gluestack-ui/config"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -11,7 +13,7 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme() || 'light';
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
@@ -27,11 +29,14 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+		<GluestackUIProvider config={config} colorMode={colorScheme}>
+			<ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+				<Stack initialRouteName='home'>
+					<Stack.Screen name="index" options={{ headerShown: false }} />
+					<Stack.Screen name="wallet/[id]" options={{ headerShown: false }} />
+					<Stack.Screen name="+not-found" />
+				</Stack>
+			</ThemeProvider>
+		</GluestackUIProvider>
   );
 }
