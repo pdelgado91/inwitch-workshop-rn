23055a916c8b20eff6f826446f0ab4d9f549b136
Paola Delgado <pdelgado@teledolar.com>
2024-11-06 08:50:18 -0600

    Se agrega switch para filtrar cuentas por monedas USD/CRC

--- components/Balances/index.tsx
+++ components/Balances/index.tsx
@@ -1,16 +1,46 @@
-import { FlatList, Text, View } from "react-native";
-import {styled} from "@gluestack-style/react"
-import {Spinner} from "@gluestack-ui/themed"
+import { FlatList, Text, View, Switch } from "react-native";
+import { styled } from "@gluestack-style/react"
+import { Spinner } from "@gluestack-ui/themed"
 import useGetBalance from "@/hooks/useGetBalance";
 import { entityMaster } from "@/utils/constant";
 import CardBalance from "../CardBalance";
+import { useEffect, useState } from "react";
 
 const WalletMasterBalance = () => {
-	const { balance, loading, error } = useGetBalance(entityMaster);
-	return(
+	const [isCRCEnabled, setIsCRCEnabled] = useState(true);
+	const [isUSDEnabled, setIsUSDEnabled] = useState(true);
+	const toggleCRCSwitch = () => setIsCRCEnabled(previousState => !previousState);
+	const toggleUSDSwitch = () => setIsUSDEnabled(previousState => !previousState);
+	var { balance, loading, error } = useGetBalance(entityMaster);
+
+	if (isCRCEnabled === false) {
+		balance = balance.filter((data) => data?.balance?.currency != "CRC");
+	}
+
+	if (isUSDEnabled === false) {
+		balance = balance.filter((data) => data?.balance?.currency != "USD");
+	}
+
+	return (
 		<Container>
 			{loading && <Spinner size={"small"} />}
 			{error && <Text>Error: {error.message}</Text>}
+			<Text>Mostrar Cuentas Colones</Text>
+			<Switch
+				trackColor={{ false: '#767577', true: '#81b0ff' }}
+				thumbColor={isCRCEnabled ? '#f5dd4b' : '#f4f3f4'}
+				ios_backgroundColor="#3e3e3e"
+				onChange={toggleCRCSwitch}
+				value={isCRCEnabled}
+			/>
+			<Text>Mostrar Cuentas Dólares</Text>
+			<Switch
+				trackColor={{ false: '#767577', true: '#81b0ff' }}
+				thumbColor={isUSDEnabled ? '#f5dd4b' : '#f4f3f4'}
+				ios_backgroundColor="#3e3e3e"
+				onChange={toggleUSDSwitch}
+				value={isUSDEnabled}
+			/>
 			<FlatList
 				showsVerticalScrollIndicator={false}
 				data={balance}
@@ -34,6 +64,7 @@ const Container = styled(View, {
 	rounded: "$xl",
 	borderWidth: "$1",
 	borderColor: "$borderLight300",
+	alignItems: 'flex-start'
 })
 
 export default WalletMasterBalance
